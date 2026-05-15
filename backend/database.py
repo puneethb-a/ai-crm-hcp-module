1ce819c35771575e50604c00331434f30890526a
puneeth <anything888z@gmail.com>
2026-05-15 13:22:07 +0530

    Fixed Railway MySQL connection

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = DATABASE_URL = "mysql://root:JTonbhPRBGLhjUhJIQtGKuzEMrwGrjRW@mysql.railway.internal:3306/railway"
-
+DATABASE_URL = "mysql+pymysql://root:JTonbhPRBGLhjUhJIQtGKuzEMrwGrjRW@yamanote.proxy.rlwy.net:11422/railway"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
